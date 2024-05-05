45110f3b090ee8209286e5cf17c1a635993af106
Toalima Tamasese <ttamases@terpmail.umd.edu>
2024-05-05 17:47:56 -0400

    created loop to go through the players turns, and in the process of creating loop and if statements in order to go through each position of the  chosen word to see if the player guess matches

--- consolidation.py
+++ consolidation.py
@@ -24,13 +24,49 @@ def word_guessing_game():
 
     # welcoming user the game + getting number of players
     print("Welcome to the Word Guessing Game!")
-    number_of_players = int(input("How many players: "))
-    total_players = number_of_players
+    total_players = int(input("How many players: "))
 
     print("Now choosing word...")
     time.sleep(3)
     print("The word has been chosen. Hint: it has", len(chosen_word), "letters")
+
+    # loop through the players + setting up counter 
+    for player in range(1, total_players + 1):
+        attemps = 3
+        letter_guess = 0
+        word_guess = 0
+        guessed_letters = []
+        while (attemps > 0):
+            underscore_word = ''
+            for letter in chosen_word:
+                if (letter in guessed_letters):
+                    underscore_word += letter
+                else:
+                    underscore_word += '_'
+            print("Guess:", underscore_word)
     
+            # the users guess and attempts 
+            player_guess = input("Guess a letter: ").lower()
+            # user guess must be 1
+            if (player_guess == 1):
+                if player_guess in guessed_letters:
+                    print("You have already guessed that letter. Try again.")
+                    continue
+                
+                correct_position = 0
+                wrong_position = 0
+                correct_guess_wrong_position = 0
+
+                for i in range(len(chosen_word)):
+                    char = chosen_word[i]
+                    if char == player_guess:
+                        if underscore_word[i] == "_":
+                            correct_position += 1
+                        else:
+                            wrong_position += 1
+
+
+
 
 
 
