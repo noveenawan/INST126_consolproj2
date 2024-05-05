3cfb0b146ceb80ad4882d4214504504b49364632
Toalima Tamasese <ttamases@terpmail.umd.edu>
2024-05-05 19:35:39 -0400

    removed outer while loop to see if program will end

--- consolidation.py
+++ consolidation.py
@@ -34,56 +34,60 @@ def word_guessing_game():
     player_scores = {}
     game_over = False
 
-    while not game_over:
     # loop through the players + setting up player turns/switching
-        for player in range(1, total_players + 1):
-            player_scores[player] = {"letter_guesses": 0, "word_guesses": 0}
-            print(f"It is now Player {player}'s turn to guess: ")
-            attemps = 3
-            guessed_letters = []
-            guessed_word = False
+    for player in range(1, total_players + 1):
+        player_scores[player] = {"letter_guesses": 0, "word_guesses": 0}
+        print(f"It is now Player {player}'s turn to guess: ")
+        attemps = 3
+        guessed_letters = []
+        guessed_word = False
 
-            while (attemps > 0):
-                underscore_word = ''
-                for letter in chosen_word:
-                    if (letter in guessed_letters):
-                        underscore_word += letter
-                    else:
-                        underscore_word += '_'
-                print("Guess the word:", ' '.join(underscore_word))
-        
-                # the users guess and attempts 
-                player_guess = input("Guess a letter: ").lower()
-
-                # user guess must be 1
-                if (len(player_guess) == 1):
-                    if player_guess in guessed_letters:
-                        print("You have already guessed that letter. Try again.")
-                    
-                    else:
-                        guessed_letters.append(player_guess)
-                        player_scores[player]["letter_guesses"] += 1
-                        letter_count = chosen_word.count((player_guess))
-                        print(f"The letter '{player_guess}' appears {letter_count} times in the word.")
-
-                elif ((len(player_guess)) == len(chosen_word)):
-                    if (player_guess == chosen_word):
-                        print("You have guessed the word correctly!!!")
-                        guessed_word = True
-                        break
-                    else:
-                        print("You guess is incorrect. Try again :( )")
-                        attemps -= 1
-                        player_scores[player]["word_guesses"] += 1
-                        if attemps == 0:
-                            print("You have no more attempts. Try again next time!")
-                            break
+        while (attemps > 0):
+            underscore_word = ''
+            for letter in chosen_word:
+                if (letter in guessed_letters):
+                    underscore_word += letter
                 else:
-                    print("Please re-enter your input.") 
+                    underscore_word += '_'
+            print("Guess the word:", ' '.join(underscore_word))
+        
+            # the users guess and attempts 
+            player_guess = input("Guess a letter: ").lower()
+
+            # user guess must be 1
+            if (len(player_guess) == 1):
+                if player_guess in guessed_letters:
+                    print("You have already guessed that letter. Try again.")
+                    
+                else:
+                    guessed_letters.append(player_guess)
+                    player_scores[player]["letter_guesses"] += 1
+                    letter_count = chosen_word.count((player_guess))
+                    print(f"The letter '{player_guess}' appears {letter_count} times in the word.")
+
+            elif ((len(player_guess)) == len(chosen_word)):
+                if (player_guess == chosen_word):
+                    print("You have guessed the word correctly!!!")
+                    guessed_word = True
+                    break
+                else:
+                    print("You guess is incorrect. Try again :( )")
+                    attemps -= 1
+                    player_scores[player]["word_guesses"] += 1
+                    if attemps == 0:
+                        print("You have no more attempts. Try again next time!")
+                        break
+            else:
+                print("Please re-enter your input.") 
             
-            if guessed_word or attemps == 0:
-                game_over = True
-                break
+        if guessed_word or attemps == 0:
+            game_over = True
+            break
+
+    print("Thanks for playing :) ")
+    for player, player_scores in player_scores.items():
+        final_score = player_scores["letter_guesses"]
+        print(f"Player {player}: {final_score} letter guesses")
 
         
 # word guessing game test case 
